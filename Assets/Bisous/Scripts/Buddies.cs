2f108288df5b3327f7d38fa5622bd95b933f76cd
JustineVuillemot <vuillemotjustine@gmail.com>
2017-12-16 17:10:15 +0100

    game manager

--- Assets/Bisous/Scripts/Buddies.cs
+++ Assets/Bisous/Scripts/Buddies.cs
@@ -5,184 +5,185 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Buddies : MonoBehaviour {
+public class Buddies : MonoBehaviour
+{
 
-	public Camera cameraComponent;
+    public Camera cameraComponent;
 
-	public Shader headShader;
-	public Shader bodyShader;
+    public Shader headShader;
+    public Shader bodyShader;
 
-	public Texture kissTexture;
+    public Texture kissTexture;
 
-	[Header("Boid Area")]
-	public float followRange = 3f;
+    [Header("Boid Area")]
+    public float followRange = 3f;
 
-	[Header("Boid Velocity")]
-	public float velocitySpeed = .1f;
-	public float velocityFriction = .9f;
-	public float velocityDamping = .1f;
-	public float velocityMax = 1f;
-
-    public bool playing = false;
-    public GameObject startButton;
-    public GameObject startImage;
-    public Text countText;
-
-    private int countKiss = 0;
+    [Header("Boid Velocity")]
+    public float velocitySpeed = .1f;
+    public float velocityFriction = .9f;
+    public float velocityDamping = .1f;
+    public float velocityMax = 1f;
 
 
-	private List<Buddy> buddyList;
-	private List<Texture2D> headTextures;
-	private List<Texture2D> bodyTextures;
-	private string[] fileHeadPaths;
-	private string[] fileBodyPaths;
-	private string dataHeadPath;
-	private string dataBodyPath;
-	private int total;
-	private RaycastHit raycast;
-	private Ray ray;
-	private float mouseClick;
+    [NonSerialized]public List<Buddy> buddyList;
+    private List<Texture2D> headTextures;
+    private List<Texture2D> bodyTextures;
+    private string[] fileHeadPaths;
+    private string[] fileBodyPaths;
+    private string dataHeadPath;
+    private string dataBodyPath;
+    private int total;
+    private RaycastHit raycast;
+    private Ray ray;
+    private float mouseClick;
 
-	void Start () {
-		dataHeadPath = Application.dataPath;
-		List<string> paths = new List<string>(dataHeadPath.Split('/'));
-		paths.RemoveAt(paths.Count-1);
-		dataHeadPath = String.Join("/", paths.ToArray());
-		dataHeadPath = System.IO.Path.Combine(dataHeadPath, "Heads/");
+    void Start()
+    {
+        dataHeadPath = Application.dataPath;
+        List<string> paths = new List<string>(dataHeadPath.Split('/'));
+        paths.RemoveAt(paths.Count - 1);
+        dataHeadPath = String.Join("/", paths.ToArray());
+        dataHeadPath = System.IO.Path.Combine(dataHeadPath, "Heads/");
 
-		dataBodyPath = Application.dataPath;
-		paths = new List<string>(dataBodyPath.Split('/'));
-		paths.RemoveAt(paths.Count-1);
-		dataBodyPath = String.Join("/", paths.ToArray());
-		dataBodyPath = System.IO.Path.Combine(dataBodyPath, "Bodies/");
+        dataBodyPath = Application.dataPath;
+        paths = new List<string>(dataBodyPath.Split('/'));
+        paths.RemoveAt(paths.Count - 1);
+        dataBodyPath = String.Join("/", paths.ToArray());
+        dataBodyPath = System.IO.Path.Combine(dataBodyPath, "Bodies/");
 
 
-		buddyList = new List<Buddy>();
-		mouseClick = 0f;
-		Fetch();
+        buddyList = new List<Buddy>();
+        mouseClick = 0f;
+        Fetch();
 
         InvokeRepeating("Direction", 0, 3);
         InvokeRepeating("RetourZero", 0, 21);
     }
 
-    public void Playing()
+    void Update()
     {
-        playing = true;
-        startButton.SetActive(false);
-        startImage.SetActive(false);
 
-    }
+        float mouseDelta = 0f;
+        if (Input.GetMouseButton(0) && GetComponent<GameManager>().playing) mouseDelta = Mathf.Clamp01(mouseDelta + 5f);
 
-    void Update () {
-
-		float mouseDelta = 0f;
-		if (Input.GetMouseButton(0) && playing) mouseDelta = Mathf.Clamp01(mouseDelta + 5f);
-
-		if (Input.GetMouseButtonDown(0) && playing) {
-			ray = cameraComponent.ScreenPointToRay(Input.mousePosition);
-			if (Physics.Raycast(ray, out raycast, 100f)) {
-				GameObject other = raycast.collider.gameObject;
-				if (other.transform.parent.parent == transform) {
-                    if(other.transform.parent.GetComponent<BuddyBehaviour>().buddy.kissed == false)
+        if (Input.GetMouseButtonDown(0) && GetComponent<GameManager>().playing)
+        {
+            ray = cameraComponent.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycast, 100f))
+            {
+                GameObject other = raycast.collider.gameObject;
+                if (other.transform.parent.parent == transform)
+                {
+                    if (other.transform.parent.GetComponent<BuddyBehaviour>().buddy.kissed == false)
                     {
-                        ++countKiss;
-                        countText.text = countKiss + "/" + buddyList.Count;
+                        GetComponent<GameManager>().UpdateCount();
                     }
-					other.transform.parent.GetComponent<BuddyBehaviour>().buddy.Kiss();
-                    
-				}
-			}
-		}
+                    other.transform.parent.GetComponent<BuddyBehaviour>().buddy.Kiss();
 
-		mouseClick = Mathf.Lerp(mouseClick, mouseDelta, .1f);
-		Shader.SetGlobalVector("_MousePosition", Input.mousePosition);
-		Shader.SetGlobalFloat("_MouseClick", mouseClick);
-		Shader.SetGlobalTexture("_KissTexture", kissTexture);
+                }
+            }
+        }
 
-		foreach (Buddy buddy in buddyList) {
+        mouseClick = Mathf.Lerp(mouseClick, mouseDelta, .1f);
+        Shader.SetGlobalVector("_MousePosition", Input.mousePosition);
+        Shader.SetGlobalFloat("_MouseClick", mouseClick);
+        Shader.SetGlobalTexture("_KissTexture", kissTexture);
 
-			Vector2 velocity = new Vector2(0,0);
+        foreach (Buddy buddy in buddyList)
+        {
 
-			Vector2 target = (buddy.targetDir - buddy.position).normalized;
+            Vector2 velocity = new Vector2(0, 0);
 
-			Vector2 avoid = new Vector2(0,0);
-			Vector2 follow = new Vector2(0,0);
-			foreach (Buddy other in buddyList) {
-				float dist = Vector2.Distance(buddy.position, other.position);
-				float radius = buddy.size + other.size;
-				Vector2 dir = buddy.position-other.position;
-				if (dist < radius && dist > 0.0001f) {
-					avoid += dir.normalized * Mathf.Max(buddy.size, dir.magnitude);
-				}
-				if (dist < followRange && dist > 0.0001f) {
-					follow += other.velocity;
-				}
-			}
+            Vector2 target = (buddy.targetDir - buddy.position).normalized;
 
-			velocity += avoid * buddy.avoidScale;
-			velocity += follow * buddy.followScale;
-			velocity += target * buddy.targetScale;
-			velocity = velocity.normalized * Mathf.Min(velocityMax, velocity.magnitude);
+            Vector2 avoid = new Vector2(0, 0);
+            Vector2 follow = new Vector2(0, 0);
+            foreach (Buddy other in buddyList)
+            {
+                float dist = Vector2.Distance(buddy.position, other.position);
+                float radius = buddy.size + other.size;
+                Vector2 dir = buddy.position - other.position;
+                if (dist < radius && dist > 0.0001f)
+                {
+                    avoid += dir.normalized * Mathf.Max(buddy.size, dir.magnitude);
+                }
+                if (dist < followRange && dist > 0.0001f)
+                {
+                    follow += other.velocity;
+                }
+            }
 
-			buddy.velocity = Vector2.Lerp(buddy.velocity * velocityFriction, velocity, velocityDamping);
-			buddy.position += buddy.velocity * velocitySpeed;
+            velocity += avoid * buddy.avoidScale;
+            velocity += follow * buddy.followScale;
+            velocity += target * buddy.targetScale;
+            velocity = velocity.normalized * Mathf.Min(velocityMax, velocity.magnitude);
 
-            
+            buddy.velocity = Vector2.Lerp(buddy.velocity * velocityFriction, velocity, velocityDamping);
+            buddy.position += buddy.velocity * velocitySpeed;
+
+
             buddy.Update();
 
-		}	
-	}
+        }
+    }
 
-	public void CreateBuddy (Texture2D texture) {
-		Material materialHead = new Material(headShader);
-		Material materialBody = new Material(bodyShader);
-		materialHead.mainTexture = texture;
-		materialBody.mainTexture = bodyTextures[(int)UnityEngine.Random.Range(0,bodyTextures.Count)];
-		Buddy buddy = new Buddy(materialHead, materialBody, transform);
-		buddy.lookAt = cameraComponent.transform;
-		buddyList.Add(buddy);
-	}
+    public void CreateBuddy(Texture2D texture)
+    {
+        Material materialHead = new Material(headShader);
+        Material materialBody = new Material(bodyShader);
+        materialHead.mainTexture = texture;
+        materialBody.mainTexture = bodyTextures[(int)UnityEngine.Random.Range(0, bodyTextures.Count)];
+        Buddy buddy = new Buddy(materialHead, materialBody, transform);
+        buddy.lookAt = cameraComponent.transform;
+        buddyList.Add(buddy);
+    }
 
-	public void CreateBuddies () {
-		foreach (Texture2D texture in headTextures) {
-			CreateBuddy(texture);
-		}
-	}
+    public void CreateBuddies()
+    {
+        foreach (Texture2D texture in headTextures)
+        {
+            CreateBuddy(texture);
+        }
+    }
 
-	public void Fetch ()
-	{
-		total = 0;
-		headTextures = new List<Texture2D>();
-		bodyTextures = new List<Texture2D>();
+    public void Fetch()
+    {
+        total = 0;
+        headTextures = new List<Texture2D>();
+        bodyTextures = new List<Texture2D>();
 
-		var info = new DirectoryInfo(dataHeadPath);
-		FileInfo[] fileInfos = info.GetFiles();
-		fileHeadPaths = new string[fileInfos.Length];
-		total += fileInfos.Length;
+        var info = new DirectoryInfo(dataHeadPath);
+        FileInfo[] fileInfos = info.GetFiles();
+        fileHeadPaths = new string[fileInfos.Length];
+        total += fileInfos.Length;
 
-		info = new DirectoryInfo(dataBodyPath);
-		FileInfo[] fileBodyInfos = info.GetFiles();
-		fileBodyPaths = new string[fileBodyInfos.Length];
-		total += fileBodyInfos.Length;
+        info = new DirectoryInfo(dataBodyPath);
+        FileInfo[] fileBodyInfos = info.GetFiles();
+        fileBodyPaths = new string[fileBodyInfos.Length];
+        total += fileBodyInfos.Length;
 
-		for (int i = 0; i < fileInfos.Length; ++i) {
-			fileHeadPaths[i] = "file://" + dataHeadPath + fileInfos[i].Name;
-			StartCoroutine(Load(fileHeadPaths[i], headTextures));
-		}
-		for (int i = 0; i < fileBodyInfos.Length; ++i) {
-			fileBodyPaths[i] = "file://" + dataBodyPath + fileBodyInfos[i].Name;
-			StartCoroutine(Load(fileBodyPaths[i], bodyTextures));
-		}
-	}
+        for (int i = 0; i < fileInfos.Length; ++i)
+        {
+            fileHeadPaths[i] = "file://" + dataHeadPath + fileInfos[i].Name;
+            StartCoroutine(Load(fileHeadPaths[i], headTextures));
+        }
+        for (int i = 0; i < fileBodyInfos.Length; ++i)
+        {
+            fileBodyPaths[i] = "file://" + dataBodyPath + fileBodyInfos[i].Name;
+            StartCoroutine(Load(fileBodyPaths[i], bodyTextures));
+        }
+    }
 
-	IEnumerator Load (string url, List<Texture2D> list) {
-		WWW www = new WWW(url);
-		yield return www;
-		list.Add(www.texture);
-		if (headTextures.Count + bodyTextures.Count == total) {
-			CreateBuddies();
-		}
-	}
+    IEnumerator Load(string url, List<Texture2D> list)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        list.Add(www.texture);
+        if (headTextures.Count + bodyTextures.Count == total)
+        {
+            CreateBuddies();
+        }
+    }
 
     public void Direction()
     {
@@ -191,7 +192,7 @@ public class Buddies : MonoBehaviour {
         foreach (Buddy buddy in buddyList)
         {
             buddy.targetDir = new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-2f, 1f));
-            buddy.followScale = UnityEngine.Random.Range(.6f, .8f)*(buddy.targetDir.x * buddy.targetDir.x + buddy.targetDir.y * buddy.targetDir.y);
+            buddy.followScale = UnityEngine.Random.Range(.6f, .8f) * (buddy.targetDir.x * buddy.targetDir.x + buddy.targetDir.y * buddy.targetDir.y);
         }
 
     }
---
+++ Assets/Bisous/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public bool playing = false;
+    public GameObject startButton;
+    public GameObject startImage;
+    public GameObject restartButton;
+    public Text messageFin;
+    public Text countText;
+
+    private int countKiss = 0;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playing)
+            countText.text = countKiss + "/" + GetComponent<Buddies>().buddyList.Count;
+
+        if (countKiss == GetComponent<Buddies>().buddyList.Count && countKiss != 0)
+        {
+            EndGame();
+        }
+    }
+
+    public void Playing()
+    {
+        playing = true;
+        startButton.SetActive(false);
+        startImage.SetActive(false);
+        restartButton.SetActive(false);
+        messageFin.text = "";
+
+        foreach (Buddy buddy in GetComponent<Buddies>().buddyList)
+        {
+            buddy.kissed = false;
+        }
+    }
+
+    public void EndGame()
+    {
+        playing = false;
+        restartButton.SetActive(true);
+        countKiss = 0;
+
+        messageFin.text = "Vous êtes génial, vous avez gagné !";
+
+    }
+
+    public void UpdateCount()
+    {
+        ++countKiss;
+    }
+}
